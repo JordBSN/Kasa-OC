ef52f9c05386c701f17700ed0bdb9d089a76490a
jordanbastin <jord.bastin@gmail.com>
2023-03-03 11:41:34 +0100

    ajout de la fonction currentYear pour la mise à jour automatique de l'année dans le footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,29 @@
+import React, { useState, useEffect } from "react";
 import styles from "./footer.module.scss";
 import LogoFooter from "../../assets/images/LogoFooter.svg";
+
+function CurrentYear() {
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setYear(new Date().getFullYear());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <span>{year}</span>;
+}
 
 function Index() {
   return (
     <footer>
       <div className={`${styles.footerContainer}`}>
         <img src={LogoFooter} alt="Logo" />
-        <span>&copy; 2022 Kasa. Tous droits réservés</span>
+        <span>&copy; {CurrentYear()} Kasa. Tous droits réservés</span>
       </div>
     </footer>
   );
